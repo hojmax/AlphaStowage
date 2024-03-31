f2a798ac4ad00bac38df4c17d2fbd709385668a4
hojmax <axelhojmark@gmail.com>
2024-03-31 20:58:38 +0200

    Update modi.sh

--- modi.sh
+++ modi.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 #SBATCH --job-name=diffusion
-#SBATCH --output=%j.txt  # Use %j to denote the job ID
+#SBATCH --output=%j.txt
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=6
 
 # Define the directory where Conda packages will be stored
 export CONDA_PKGS_DIRS=~/modi_mount/conda_dir
 
-# Define the Conda directory (adjust if necessary)
-export CONDA_DIR=/opt/conda
+# Add Conda's condabin to PATH
+export PATH="/opt/conda/condabin:$PATH"
 
-# Initialize Conda
-source $CONDA_DIR/etc/profile.d/conda.sh
-
-# Activate the environment, or create it if it doesn't exist
+# Attempt to activate the environment, or create it if it doesn't exist
 conda activate myenv
 if [ $? -ne 0 ]; then
     conda create -n myenv -y python=3.8
     conda activate myenv
 fi
-
 
 echo "*** Installing requirements ***"
 
