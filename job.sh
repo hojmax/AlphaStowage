7f6fb774121166746e90908d69927ad25e17e1f3
hojmax <axelhojmark@gmail.com>
2024-04-03 14:49:33 +0200

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=rl
 #SBATCH --output=%j.txt  # Use %j to denote the job ID
-#SBATCH --gres=gpu:5
-#SBATCH --time=20:00:00
+#SBATCH --gres=gpu:8
+#SBATCH --time=30:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=15
+#SBATCH --cpus-per-task=24
 #SBATCH --mem-per-cpu=1G
 
 echo "*** Loading modules ***"
