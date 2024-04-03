1f05cdc40ab7d8f678d3e2ff628c2c64aa0c7d09
hojmax <axelhojmark@gmail.com>
2024-04-03 21:12:46 +0200

    titanrtx

--- job.sh
+++ job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=rl
 #SBATCH --output=%j.txt  # Use %j to denote the job ID
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:titanrtx:1
 #SBATCH --time=30:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=96
