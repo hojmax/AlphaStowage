b5820b894fd85222d5dcaf32556dab11d95e52f4
christianjensen2903 <cmj290300@gmail.com>
2024-05-23 15:34:06 +0200

    Revert "✨ Subtract 1 from Q instead of subtracting depth from U"
    This reverts commit 987849556f585d0f72222bdb097f6299848ef197.

--- src/Node.py
+++ src/Node.py
@@ -1,5 +1,6 @@
 import numpy as np
 from MPSPEnv import Env
+import warnings
 
 
 class TruncatedEpisodeError(Exception):
@@ -11,21 +12,33 @@ class Node:
         self.Q = 0
         self.N = 0
         self.best_depth = np.inf
-        self.U: float | None = None
+        self._U: float | None = None
         self._P: np.ndarray | None = None
-
-        # action -> (child, number of edge_visits)
-        self.children_and_edge_visits: dict[int, tuple["Node", int]] = {}
+        self._children_and_edge_visits: dict[int, tuple["Node", int]] = {}
 
         self.game_state = game_state
 
+    @property
+    def children_and_edge_visits(self) -> dict[int, tuple["Node", int]]:
+        """action -> (child, number of edge_visits)"""
+
+        # Filter out children where there is a quicker path to the same state
+        valid_children = {}
+        for action, (child, edge_visits) in self._children_and_edge_visits.items():
+            has_valid_children = child.P is not None and child.P.sum() > 0
+            is_quickest_path = child.best_depth > self.best_depth
+            if has_valid_children and is_quickest_path:
+                valid_children[action] = (child, edge_visits)
+
+        return valid_children
+
     def add_child(self, action: int, child: "Node") -> None:
-        self.children_and_edge_visits[action] = (child, 0)
+        self._children_and_edge_visits[action] = (child, 0)
 
     def increment_visits(self, action: int) -> None:
-        self.children_and_edge_visits[action] = (
-            self.children_and_edge_visits[action][0],
-            self.children_and_edge_visits[action][1] + 1,
+        self._children_and_edge_visits[action] = (
+            self._children_and_edge_visits[action][0],
+            self._children_and_edge_visits[action][1] + 1,
         )
 
     @property
@@ -34,8 +47,28 @@ class Node:
         if self._P is None:
             return None
 
-        return self._P * self.game_state.mask
+        # Filter out actions that has a quicker path to the same state
+        quickest_path = np.ones(len(self._P))
+        for action, (child, _) in self._children_and_edge_visits.items():
+            is_quickest_path = child.best_depth > self.best_depth
+            if not is_quickest_path:
+                quickest_path[action] = 0
+
+        P = self._P * quickest_path
+        P *= self.game_state.mask
+        if P.sum() != 0:
+            P /= P.sum()
+
+        return P
 
     @P.setter
     def P(self, value):
         self._P = value
+
+    @property
+    def U(self) -> float:
+        return self._U - self.best_depth if self._U else None
+
+    @U.setter
+    def U(self, value: float) -> None:
+        self._U = value
